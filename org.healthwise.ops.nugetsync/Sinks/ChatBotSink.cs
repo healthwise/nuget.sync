66bb8a250e79c7e4bbe944eb5bad06be30bf3266
Chris Holmes <cholmes@healthwise.org>
2018-09-14 09:21:58 -0600

    made all parameters lowercase

--- org.healthwise.ops.nugetsync/Sinks/ChatBotSink.cs
+++ org.healthwise.ops.nugetsync/Sinks/ChatBotSink.cs
@@ -9,19 +9,19 @@ namespace org.healthwise.ops.nugetsync
 {
     public class ChatBotSink : ILogEventSink
     {
-        private readonly string _FunctionKey;
-        private readonly string _ServiceURL;
-        private readonly string _ChannelData;
-        private readonly string _Environment;
+        private readonly string _functionKey;
+        private readonly string _serviceURL;
+        private readonly string _channelData;
+        private readonly string _environment;
         private readonly IFormatProvider _formatProvider;
 
-        public ChatBotSink(IFormatProvider formatProvider, string FunctionKey, string ServiceURL, string ChannelData, string Environment)
+        public ChatBotSink(IFormatProvider formatProvider, string functionKey, string serviceURL, string channelData, string environment)
         {
             _formatProvider = formatProvider;
-            _FunctionKey = FunctionKey;
-            _ServiceURL = ServiceURL;
-            _ChannelData = ChannelData;
-            _Environment = Environment;
+            _functionKey = functionKey;
+            _serviceURL = serviceURL;
+            _channelData = channelData;
+            _environment = environment;
         }
 
         // Construct a sink that passes messages to the specificed chat account.
@@ -34,8 +34,8 @@ namespace org.healthwise.ops.nugetsync
             JObject json = JObject.FromObject(new
             {
                 type = "message",
-                serviceURL = _ServiceURL,
-                channelData = new {teamsChannelId = _ChannelData},
+                serviceURL = _serviceURL,
+                channelData = new {teamsChannelId = _channelData},
                 message = logEvent.RenderMessage(_formatProvider)
             });
 
@@ -43,10 +43,10 @@ namespace org.healthwise.ops.nugetsync
 
             using (var client = new HttpClient())
             {
-                Uri baseUrl = new Uri($"https://hw-{_Environment}-botservice-unclehab.azurewebsites.net");
+                Uri baseUrl = new Uri($"https://hw-{_environment}-botservice-unclehab.azurewebsites.net");
                 client.BaseAddress = baseUrl; //Base part of URL (like https://api.healthwise.org)
 
-                string restOfUrl = $"/api/MessageProxy?code={_FunctionKey}";
+                string restOfUrl = $"/api/MessageProxy?code={_functionKey}";
                 var result = client.PostAsync(restOfUrl, content).Result; //Rest of url - API endpoint - /my/endpoint
 
             }
