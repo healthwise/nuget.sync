18aae6faf970eed3a0ca179eb8903a7f0fc2a13c
Chris Holmes <cholmes@healthwise.org>
2018-09-19 15:28:45 -0600

    init commit of NpmProviderFactory

---
+++ org.healthwise.ops.nugetsync/Providers/NpmProviderFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace org.healthwise.ops.nugetsync.Providers
+{
+    public class NpmProviderFactory : INuGetProviderFactory
+    {
+        public IPackageProvider LoadProvider(string providerType, string repositoryUrl, string writeToken, string readUsername,
+            string readPassword)
+        {
+                return new NpmPackageProvider(repositoryUrl, writeToken, readUsername, readPassword, providerType);
+        }
+    }
+}
